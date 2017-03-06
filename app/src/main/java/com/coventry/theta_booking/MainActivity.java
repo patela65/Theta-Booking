1f8686f32a15d9c0ac7e349623deeac4c89946e3
patela65 <muhammadmoazkhan@Muhammad-Moaz-Khans-MacBook-Pro.local>
2017-03-06 05:11:44 +0500

    initial commit
    initial commit

---
+++ app/src/main/java/com/coventry/theta_booking/MainActivity.java
@@ -0,0 +1,13 @@
+package com.coventry.theta_booking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
